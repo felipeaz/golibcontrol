46f8eaa641fccecf3ac61944d4c7424b8fdcea3b
Felipe Azevedo <32846823+FelipeAz@users.noreply.github.com>
2021-02-17 20:00:37 -0300

    Update connect.go

--- database/connect.go
+++ database/connect.go
@@ -1,9 +1,5 @@
 package database
 
-/* This package require go-sql-driver and a MYSQL Server installed.
-To install the go-sql-driver, please run the following command:
-go get -u github.com/go-sql-driver/mysql */
-
 import (
 	"github.com/FelipeAz/golibcontrol/database/schema"
 	"gorm.io/gorm"
