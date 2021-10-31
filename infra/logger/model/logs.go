45b3dda7cf156a9f52b6757039c0879428757ce7
Felipe Azevedo <felipe9_azevedo@hotmail.com>
2021-10-31 14:31:52 -0300

    Changed log order and removed testing logs

--- infra/logger/model/logs.go
+++ infra/logger/model/logs.go
@@ -5,9 +5,9 @@ import (
 )
 
 type Log struct {
-	Message string    `json:"message"`
-	Service string    `json:"service"`
-	Error   string    `json:"error"`
 	Level   string    `json:"severity"`
+	Service string    `json:"service"`
+	Message string    `json:"message"`
+	Error   string    `json:"error"`
 	Time    time.Time `json:"timestamp"`
 }
