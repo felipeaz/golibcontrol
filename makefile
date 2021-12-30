3b6421eb8919e57c972551835dc54d86c237cfb8
FelipeAz <felipe9_azevedo@hotmail.com>
2021-12-29 22:35:10 -0500

    Removed Vendor

--- makefile
+++ makefile
@@ -1,12 +1,6 @@
 install:
 	sh scripts/install.sh
 
-run:
-	sh scripts/run.sh
-
-rund:
-	sh scripts/rund.sh
-
 stop:
 	sh scripts/stop.sh
 
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,1 +1,6 @@
-cp .env_example .env && docker-compose up --build
+cp .env_example .env && docker-compose \
+-f build/docker/services/conf/docker-compose.yaml \
+-f build/docker/services/account/docker-compose.yaml \
+-f build/docker/services/management/docker-compose.yaml \
+-f build/docker/services/platform/docker-compose.yaml \
+up --build
