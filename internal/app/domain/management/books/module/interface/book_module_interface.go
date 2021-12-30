727b7abc106c9f2c702497b69a70c13b93629e78
FelipeAz <felipe9_azevedo@hotmail.com>
2021-12-29 22:49:31 -0500

    Fixed book module issue and removed vendor from go dockerfile

--- internal/app/domain/management/books/module/interface/book_module_interface.go
+++ internal/app/domain/management/books/module/interface/book_module_interface.go
@@ -9,7 +9,7 @@ type BookModuleInterface interface {
 	Get() ([]model.Book, *errors.ApiError)
 	GetByFilter(filter model.Filter) ([]model.Book, *errors.ApiError)
 	Find(id string) (model.Book, *errors.ApiError)
-	Create(book model.Book) (uint, *errors.ApiError)
+	Create(book model.Book) (*model.Book, *errors.ApiError)
 	Update(id string, upBook model.Book) *errors.ApiError
 	Delete(id string) *errors.ApiError
 }
