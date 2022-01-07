137f34f9a4aeaec50f516a3906cc85c20f6cf4f0
FelipeAz <felipe9_azevedo@hotmail.com>
2022-01-06 22:19:20 -0500

    Created docker file for each tool used

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,7 @@
 cp .env_example .env && docker-compose \
--f docker/conf.yaml \
+-f docker/redis.yaml \
+-f docker/kong.yaml \
+-f docker/kafka.yaml \
 -f docker/account.yaml \
 -f docker/management.yaml \
 -f docker/platform.yaml \
