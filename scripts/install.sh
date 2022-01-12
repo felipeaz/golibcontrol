19239d13ad1dc0ff298ddae2f392599bb9d08b94
FelipeAz <felipe9_azevedo@hotmail.com>
2022-01-12 10:06:43 -0500

    Removed Kafka from install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,6 @@
 cp .env_example .env && docker network create -d bridge lib-net && docker-compose \
 -f docker/kong.yaml \
 -f docker/redis.yaml \
--f docker/kafka.yaml \
 -f docker/account.yaml \
 -f docker/management.yaml \
 -f docker/platform.yaml \
