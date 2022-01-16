a9e6c9eff1aa541a051ec9bf5ddfbd631cadd9df
felipeaz <felipe9_azevedo@hotmail.com>
2022-01-16 16:54:36 -0500

    Added CORS plugin on gateway script

--- scripts/gateway.sh
+++ scripts/gateway.sh
@@ -30,6 +30,7 @@ curl --location --request POST 'http://localhost:8001/services/management-servic
 --data-urlencode 'methods=GET' \
 --data-urlencode 'methods=POST' \
 --data-urlencode 'methods=PUT' \
+--data-urlencode 'methods=OPTIONS' \
 --data-urlencode 'methods=DELETE'
 
 curl --location --request POST 'http://localhost:8001/services/account-service/routes' \
@@ -39,6 +40,7 @@ curl --location --request POST 'http://localhost:8001/services/account-service/r
 --data-urlencode 'methods=GET' \
 --data-urlencode 'methods=POST' \
 --data-urlencode 'methods=PUT' \
+--data-urlencode 'methods=OPTIONS' \
 --data-urlencode 'methods=DELETE'
 
 curl --location --request POST 'http://localhost:8001/services/platform-service/routes' \
@@ -48,6 +50,7 @@ curl --location --request POST 'http://localhost:8001/services/platform-service/
 --data-urlencode 'methods=GET' \
 --data-urlencode 'methods=POST' \
 --data-urlencode 'methods=PUT' \
+--data-urlencode 'methods=OPTIONS' \
 --data-urlencode 'methods=DELETE'
 
 printf "Setting up authentication...\n\n"
@@ -63,3 +66,19 @@ curl --location --request POST 'http://localhost:8001/services/management-servic
 --data-urlencode 'name=jwt' \
 --data-urlencode 'config.secret_is_base64=false' \
 --data-urlencode 'config.run_on_preflight=true'
+
+curl --location --request POST 'http://localhost:8001/plugins' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'name=cors' \
+--data-urlencode 'config.origins=*' \
+--data-urlencode 'config.methods=GET' \
+--data-urlencode 'config.methods=POST' \
+--data-urlencode 'config.methods=PUT' \
+--data-urlencode 'config.methods=DELETE' \
+--data-urlencode 'config.methods=OPTIONS' \
+--data-urlencode 'config.headers=Content-Type' \
+--data-urlencode 'config.headers=Access-Control-Allow-Origin' \
+--data-urlencode 'config.exposed_headers=Access-Control-Allow-Origin' \
+--data-urlencode 'config.credentials=true' \
+--data-urlencode 'config.preflight_continue=true' \
+--data-urlencode 'config.max_age=3600'
